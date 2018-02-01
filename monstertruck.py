9114944b9b48d28c1dc83d0f687a297ea4a54b52
Abhishek Mishra <abhishekmishra3@yahoo.com>
2018-02-01 21:39:30 +0530

    started making a monster truck

---
+++ monstertruck.py
@@ -0,0 +1,52 @@
+from turtle import *
+
+penup()
+forward(100)
+
+begin_fill()
+circle(-100)
+end_fill()
+
+right(90)
+forward(80)
+left(90)
+
+fillcolor('gray')
+begin_fill()
+circle(-20)
+end_fill()
+
+right(180)
+forward(250)
+right(90)
+forward(80)
+right(90)
+
+fillcolor('black')
+begin_fill()
+circle(-100)
+end_fill()
+
+right(90)
+forward(80)
+left(90)
+
+fillcolor('gray')
+begin_fill()
+circle(-20)
+end_fill()
+
+color('gray', 'black')
+forward(105)
+pendown()
+left(90)
+circle(105, 180)
+
+penup()
+left(90)
+forward(465)
+
+color('gray', 'black')
+pendown()
+left(90)
+circle(105, 180)
