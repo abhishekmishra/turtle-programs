786b2c2515441b15939da17ef0bf70814e400bb7
Abhishek Mishra <abhishekmishra3@yahoo.com>
2019-02-23 12:21:29 +0530

    added spiral0

---
+++ picoturtle-py/spiral0.py
@@ -0,0 +1,25 @@
+from picoturtle import *
+create_turtle()
+
+### Your code goes here ###
+
+def spiral(distance, angle, pwidth, distance_inc, angle_inc, pwidth_inc):
+    x = distance
+    a = angle
+    pw = pwidth
+    for i in range(100):
+        penwidth(pw)
+        forward(x)
+        right(a)
+        x += distance_inc
+        a += angle_inc
+        pw += pwidth_inc
+
+pencolour(128, 128, 0)
+pendown()
+spiral(1, 45, 1, 1, 0, 0.1)
+
+### Your code ends here ###
+
+# Always stop the turtle
+stop()
