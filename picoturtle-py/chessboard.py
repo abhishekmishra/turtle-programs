626cf6f79515b7b6e4a9fb89f17aa916890b4581
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-02-23 22:05:36 +0530

    added chessboard

---
+++ picoturtle-py/chessboard.py
@@ -0,0 +1,65 @@
+from picoturtle import *
+create_turtle()
+
+### Your code goes here ###
+
+def dark_square(side):
+    penwidth(side)
+    forward(side)
+
+def square(side):
+    penwidth(1)
+    for i in range(4):
+        forward(side)
+        right(90)
+
+left(90)
+forward(100)
+right(90)
+forward(-200)
+
+pencolour(165, 42, 42)
+for i in range(4):
+    for i in range(4):
+        pendown()
+        dark_square(30)
+        penup()
+        left(90)
+        forward(15)
+        right(90)
+        pendown()
+        square(30)
+        penup()
+        right(90)
+        forward(15)
+        left(90)
+        forward(30)
+        penup()
+    forward(-240)
+    right(90)
+    forward(30)
+    left(90)
+    for i in range(4):
+        left(90)
+        forward(15)
+        right(90)
+        pendown()
+        square(30)
+        penup()
+        right(90)
+        forward(15)
+        left(90)
+        forward(30)
+        penup()
+        pendown()
+        dark_square(30)        
+        penup()
+    forward(-240)
+    right(90)
+    forward(30)
+    left(90)
+
+### Your code ends here ###
+
+# Always stop the turtle
+stop()
