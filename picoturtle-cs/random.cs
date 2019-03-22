496e2ef9130911d1a1294db30b4172b91139440e
Abhishek Mishra <abhishekmishra3@gmail.com>
2019-03-22 13:51:56 +0530

    updated random

--- picoturtle-cs/random.cs
+++ picoturtle-cs/random.cs
@@ -13,11 +13,24 @@ namespace cspico
             if (t != null) {
                 // Your code goes here
 
+                t.penup();
+                t.pencolour(0, 255, 255);
+                t.sety(0);
+                t.setx(0);
+                t.right(90);
+                t.font("12pt Courier");
+                t.filltext("random turtle");
+                
+                t.home();
                 t.pendown();
-                t.pencolour(255, 0, 0);
+                t.penwidth(4);
+
                 var r = new Random();
                 for(int i = 0; i < 100; i++) {
-                    t.forward(r.Next(0, 10));
+                    t.pencolour(r.Next(0, 255), 
+                                r.Next(0, 255), 
+                                r.Next(0, 255));
+                    t.forward(r.Next(0, 20));
                     t.right(r.Next(-90, 90));
                 }
 
